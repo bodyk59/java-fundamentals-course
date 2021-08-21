4a7d102ae2e2c2feba92ff967afb664498a9c807
Bohdan_Kurchak <vobdoj-tUtgog-xirqi5>
2021-08-21 09:59:43 +0300

    Homework 2: 1-3-0 / 1-5-0

--- 1-0-java-basics/1-5-0-hello-annotations/src/main/java/com/bobocode/basics/HelloAnnotationsExercise.java
+++ 1-0-java-basics/1-5-0-hello-annotations/src/main/java/com/bobocode/basics/HelloAnnotationsExercise.java
@@ -1,7 +1,7 @@
 package com.bobocode.basics;
 
 /**
- * {@link HelloAnnotationsExercise} is an exercise class that should be annotation with appropriate @{@link Exercise}
+ * {@link HelloAnnotationsExercise} is an exercise class that should be annotated with appropriate @{@link Exercise}
  * annotation. With that annotation it should specify exercise name "hello-annotation-basic" and the default complexity
  * level.
  * <p>
@@ -9,5 +9,6 @@ package com.bobocode.basics;
  * todo: Add String value that will store exercise name
  * todo: Add complexityLevel with a default {@link Level} basic
  */
+@Exercise("hello-annotation-basic")
 public class HelloAnnotationsExercise { // todo: mark class with the annotation according to the javadoc
 }
